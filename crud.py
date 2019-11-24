68bf6752e87cddc90a67242bdfb45dbbcc2884ab
Isaac Anderson <kavunshiva@gmail.com>
2019-11-24 16:04:58 -0500

    add error response, only append to object_alerts if object exists in DB, move DB logic to database.py

--- crud.py
+++ crud.py
@@ -1,9 +1,9 @@
 from typing import List
-from fastapi.encoders import jsonable_encoder
 
 from app import app
-from database import database, objects, alerts, object_alerts
-from models import ObjectIn, AlertIn, Alert
+from fastapi import HTTPException
+from database import *
+from models import ObjectIn, AlertIn
 from websocket import message_queue
 
 @app.post('/object', response_model=ObjectIn)
@@ -13,59 +13,26 @@ async def create_object(object: ObjectIn):
         phone_number=object.phone_number,
     )
     id = await database.execute(query)
+    await _trigger_any_error_messages(id, 'objects')
     return {**object.dict(), 'id': id}
 
 @app.post('/alert', response_model=Alert)
 async def create_alert(alert: AlertIn):
-    object_ids = await _get_object_ids(alert)
-    new_alert_params = await _insert_alert(alert, len(object_ids))
-    await _update_join_table(new_alert_params['id'], object_ids)
+    object_ids = await get_object_ids(alert)
+    new_alert_params = await insert_alert(alert, len(object_ids))
+    new_alert_id = new_alert_params['id']
+    await _trigger_any_error_messages(new_alert_id, 'alerts')
+    await update_join_table(new_alert_id, object_ids)
     _add_alert_to_websocket_queue(new_alert_params, object_ids)
     return new_alert_params
 
-async def _get_object_ids(alert: AlertIn):
-    if alert.alert_all:
-        object_ids = []
-        for id in jsonable_encoder(
-            await database.fetch_all('SELECT id FROM objects')
-        ):
-            object_ids.append(id['id'])
-        alert.object_ids = object_ids
-        return object_ids
-    else:
-        return alert.object_ids
-
-async def _insert_alert(alert: AlertIn, object_count: int):
-    query = alerts.insert().values(
-        text=alert.text,
-        alert_all=alert.alert_all,
-        number_of_objects_receiving_alert=object_count,
-    )
-    return {
-        **_alert_params(alert),
-        'number_of_objects_receiving_alert': object_count,
-        'id': await database.execute(query),
-    }
-
-def _alert_params(alert: AlertIn):
-    alert_params = {}
-    alert_class_keys = Alert.__dict__['__fields__'].keys()
-    for k, v in alert.dict().items():
-        if k in alert_class_keys: alert_params[k] = v
-    return alert_params
-
-async def _update_join_table(alert_id: int, object_ids: List[int]):
-    join_table_query = [
-        object_alerts.insert(),
-        await _object_alert_params(alert_id, object_ids),
-    ]
-    return await database.execute_many(*join_table_query)
-
-async def _object_alert_params(alert_id: int, object_ids: List[int]):
-    params = []
-    for object_id in object_ids:
-        params.append({'object_id': object_id, 'alert_id': alert_id})
-    return params
+async def _trigger_any_error_messages(id: int, resource: str):
+    if id not in (await get_all_ids(resource)):
+        raise HTTPException(
+            status_code=404,
+            detail=f'No {resource.title()} with ID {id} found.',
+        )
+    return None
 
 def _add_alert_to_websocket_queue(alert_params: dict, object_ids: List[int]):
     global message_queue
--- database.py
+++ database.py
@@ -1,7 +1,10 @@
+from typing import List
 import databases
 import sqlalchemy
+from fastapi.encoders import jsonable_encoder
 
 from app import app
+from models import AlertIn, Alert
 from settings import DATABASE_URL
 
 database = databases.Database(DATABASE_URL)
@@ -34,7 +37,8 @@ object_alerts = sqlalchemy.Table(
 )
 
 engine = sqlalchemy.create_engine(DATABASE_URL)
-metadata.create_all(engine)
+
+metadata.create_all(engine) # TODO: move this into a separate migration script
 
 @app.on_event('startup')
 async def startup():
@@ -43,3 +47,67 @@ async def startup():
 @app.on_event('shutdown')
 async def shutdown():
     await database.disconnect()
+
+async def get_all_ids(resource: str):
+    query = select_all_ids_from_resource_query(resource)
+    ids = []
+    for id in jsonable_encoder(await database.fetch_all(query)):
+        ids.append(id['id'])
+    return ids
+
+def select_all_ids_from_resource_query(resource: str):
+    # avoid SQL injection (:resource interpolation doesn't work w/ table names)
+    return {
+        'objects': 'SELECT id FROM objects',
+        'alerts': 'SELECT id FROM alerts',
+    }[resource]
+
+async def get_object_ids(alert: AlertIn):
+    if alert.alert_all:
+        object_ids = await get_all_ids('objects')
+        alert.object_ids = object_ids
+        return object_ids
+    else:
+        # return all object IDs on the alert existing in the objects table
+        return await persisted_object_ids(alert.object_ids)
+
+async def persisted_object_ids(ids: List[int]):
+    all_db_ids = {}
+    for id in (await get_all_ids('objects')):
+        all_db_ids[id] = True
+    persisted_ids = []
+    for id in ids:
+        if id in all_db_ids: persisted_ids.append(id)
+    return persisted_ids
+
+async def insert_alert(alert: AlertIn, object_count: int):
+    query = alerts.insert().values(
+        text=alert.text,
+        alert_all=alert.alert_all,
+        number_of_objects_receiving_alert=object_count,
+    )
+    return {
+        **alert_params(alert),
+        'number_of_objects_receiving_alert': object_count,
+        'id': await database.execute(query),
+    }
+
+def alert_params(alert: AlertIn):
+    params = {}
+    alert_class_keys = Alert.__dict__['__fields__'].keys()
+    for k, v in alert.dict().items():
+        if k in alert_class_keys: params[k] = v
+    return params
+
+async def update_join_table(alert_id: int, object_ids: List[int]):
+    join_table_query = [
+        object_alerts.insert(),
+        await object_alert_params(alert_id, object_ids),
+    ]
+    return await database.execute_many(*join_table_query)
+
+async def object_alert_params(alert_id: int, object_ids: List[int]):
+    params = []
+    for object_id in object_ids:
+        params.append({'object_id': object_id, 'alert_id': alert_id})
+    return params
